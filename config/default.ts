ada9d4f0ec2db6714361f508112a1d4ce918b553
John Koen <jkoen@smartronix.com>
2023-02-25 15:41:38 -0500

    Added initial nodemailer config

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,11 @@ export default {
   port: 3000,
   dbUri: 'mongodb://localhost:27017/node-express-api-template',
   logLevel: 'info',
+  smtp: {
+    user: 'llgmi7ikitodde7z@ethereal.email',
+    pass: 'E77kuQVWXJ4HMeSBWy',
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false
+  }
 };
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { CreateUserInput } from './user.schema';
 import { createUser } from './user.service';
+import sendEmail from '../utils/mailer';
 
 export async function createUserHander(
   req: Request<{}, {}, CreateUserInput>,
@@ -10,6 +11,12 @@ export async function createUserHander(
 
   try {
     const user = await createUser(body);
+    await sendEmail({
+      to: user.email,
+      from: 'john@johnkoen.com',
+      subject: 'Please verify your account',
+      text: `verification code: ${user.verificationCode} Id: ${user._id}`
+    });
     return res.send('User successfully created');
   } catch (e: any) {
     return res.status(500).send(e);
