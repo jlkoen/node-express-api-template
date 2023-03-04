4fcf8de2bd485cca7c92cf0885b7b2bb587c8523
John Koen <jkoen@smartronix.com>
2023-03-04 12:13:47 -0500

    Added morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import router from './app.route';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'prod') {
+  app.use(morgan('combined'));
+} else if (process.env.NODE_ENV === 'dev') {
+  app.use(morgan('dev'));
+}
 
 app.use(router);
 
